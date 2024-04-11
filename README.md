243f40fc4169945d10ad4180b3322835874c4545
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2024-04-11 23:20:11 +0800

    Add Top navigation for md up screen sizes bar to show the selected category in Portfolio page, Update ReadMe to show how to setup local for people who wants to clone

--- README.md
+++ README.md
@@ -14,6 +14,21 @@ pnpm dev
 bun dev
 ```
 
+3. **Setup .env file**
+   ```js
+    MONGODB=
+    GOOGLE_CLIENT_ID=
+    GOOGLE_CLIENT_SECRET=
+    NEXTAUTH_SECRET=
+    NEXTAUTH_URL="http://localhost:3000"
+    NEXT_PUBLIC_APP_URL="http://localhost:3000"
+
+    NEXT_PUBLIC_NEWS_DATA_API_KEY='this is an api coming from newsdata.io'
+
+    NEXT_PUBLIC_INFURA_ID='https://mainnet.infura.io/v3/api-key'
+    NEXT_PUBLIC_METAMASK_ADDRESS='0xGetYourOwnMetaMaskAddress'
+   ```
+
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -12,6 +12,11 @@ import {
 } from '@heroicons/react/24/outline'
 import { useState } from 'react'
 import { Montserrat } from 'next/font/google'
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  XMarkIcon
+} from '@heroicons/react/24/solid'
 
 const montserrat = Montserrat({ subsets: ['latin'], display: 'swap' })
 
@@ -41,34 +46,90 @@ const Portfolio = () => {
     <main
       className={`flex overflow-hidden h-full w-full xl:max-w-[1024px] sm:pt-6 xl:pt-12 lg:max-w-[924px] mx-auto sm:px-12 lg:px-0 ${montserrat.className} my-2 sm:my-0`}
     >
-      <div className='flex flex-grow h-full rounded-xl bg-stone-200 flex-col shadow-xl'>
-        <div className='px-4 xs:pt-5 hidden xs:pb-3 xs:flex gap-x-4 items-center hover:bg-[#F5F5F5]'>
-          <div className='block'>
-            <div className='relative h-[60px] w-[60px]'>
-              <Image
-                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-                src={'/images/icons/portfolio-thumbnail.png'}
-                fill
-                priority
-                alt='Portfolio Thumbnail'
-              />
+      <div className='flex flex-grow h-full rounded-xl bg-stone-200/95 flex-col shadow-xl overflow-hidden'>
+        <div className='flex border-b-[0.3px] border-gray-400/60 sm:border-gray-300/90 border-solid'>
+          <div className='px-4 xs:pt-5 hidden xs:pb-3 xs:flex gap-x-4 items-center hover:bg-[#F5F5F5] md:w-1/3 shadow-sm'>
+            <div className='block'>
+              <div className='relative h-[60px] w-[60px]'>
+                <Image
+                  sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                  src={'/images/icons/portfolio-thumbnail.png'}
+                  fill
+                  priority
+                  alt='Portfolio Thumbnail'
+                />
+              </div>
+            </div>
+            <div className='flex flex-col'>
+              <h1 className='font-medium text-base text-black'>Portfolio</h1>
+              <p className='font-normal text-xs text-gray-700 tracking-tight'>
+                Get to know about Jerald here.
+              </p>
             </div>
           </div>
-          <div className='flex flex-col'>
-            <h1 className='font-medium text-base text-black'>Portfolio</h1>
-            <p className='font-normal text-xs text-gray-700 tracking-tight'>
-              Get to know about Jerald here.
-            </p>
+          <div
+            className={`flex-1 px-4 pt-2.5 pb-3 hidden md:flex gap-x-4 h-[92px] flex-col justify-between items-center bg-white/95 overflow-hidden ${
+              activeCategory !== null ? 'shadow' : ''
+            }`}
+          >
+            <div className='flex gap-1'>
+              {icons.map((_, i) => (
+                <button
+                  className={`h-1.5 w-1.5 rounded-full ${
+                    activeCategory === i ? 'bg-gray-500' : 'bg-gray-400'
+                  }`}
+                  key={_.title}
+                  onClick={() => setActiveCategory(i)}
+                />
+              ))}
+            </div>
+            {activeCategory != null && (
+              <div className='flex items-center justify-between h-11 w-full'>
+                <div className='flex gap-x-1 w-12 justify-between'>
+                  {activeCategory > 0 ? (
+                    <ChevronLeftIcon
+                      className={`text-blue-600 hover:text-blue-800 h-5 w-5 cursor-pointer flex-1
+                    }`}
+                      onClick={() => setActiveCategory(activeCategory - 1)}
+                    />
+                  ) : (
+                    <span className='h-5 w-5 flex-1' />
+                  )}
+
+                  {activeCategory < icons.length - 1 ? (
+                    <ChevronRightIcon
+                      className={`text-blue-600 hover:text-blue-800 h-5 w-5 cursor-pointer flex-1
+                    }`}
+                      onClick={() => setActiveCategory(activeCategory + 1)}
+                    />
+                  ) : (
+                    <span className='h-5 w-5 flex-1' />
+                  )}
+                </div>
+                <h2 className='font-semibold text-lg text-black'>
+                  {icons[activeCategory].title}
+                </h2>
+                <div className='flex w-12 justify-end'>
+                  <XMarkIcon
+                    className='text-blue-600 h-5 w-5 hover:text-blue-800'
+                    onClick={() => setActiveCategory(null)}
+                  />
+                </div>
+              </div>
+            )}
           </div>
         </div>
-        <hr className='border-[0.3px] border-gray-300 border-solid hidden xs:block' />
         {/* start  */}
         <div className='flex-col flex md:flex-row sm:justify-between h-full overflow-hidden'>
-          <div className='flex items-center justify-between sm:order-2 md:order-[initial] md:w-1/3 lg:w-1/4 md:justify-center md:border-r-[0.3px] md:border-r-gray-300 md:items-baseline'>
-            <div className='flex flex-row md:flex-col py-1 items-baseline gap-x-6 justify-evenly w-full'>
+          <div className='flex items-center justify-between sm:order-2 md:order-[initial] md:w-1/3 md:justify-center md:border-r-[0.3px] md:border-r-gray-300 md:items-baseline'>
+            <div className='flex flex-row md:flex-col py-1 md:py-0 items-baseline gap-x-6 justify-evenly w-full md:mt-6 md:mx-4 md:bg-white md:rounded-xl md:pl-4'>
               {icons.map((icon, index) => (
                 <button
-                  className='grid place-content-center place-items-center gap-y-1.5 xs:w-[62px] cursor-pointer md:flex md:items-center md:justify-between group md:w-full md:flex-row-reverse md:p-6 md:border-b-[0.3px] md:border-gray-300 md:border-solid'
+                  className={`grid place-content-center place-items-center gap-y-1.5 xs:w-[62px] cursor-pointer md:flex md:items-center md:justify-between group md:w-full md:flex-row-reverse md:px-6 md:py-3.5 md:pr-3 md:pl-0 ${
+                    index < icons.length - 1
+                      ? 'md:border-b md:border-[#3C3C43]/36 mdd:border-solid'
+                      : ''
+                  }`}
                   key={icon.title}
                   onClick={() =>
                     activeCategory === index
@@ -76,7 +137,7 @@ const Portfolio = () => {
                       : setActiveCategory(index)
                   }
                 >
-                  <span className='rounded-full bg-white group-hover:bg-stone-50 grid place-items-center p-4 xs:w-[62px] xs:h-[62px] w-10 h-10 place-content-center'>
+                  <span className='rounded-full bg-white group-hover:bg-stone-50 grid place-items-center p-4 xs:w-[62px] xs:h-[62px] w-10 h-10 md:h-4 md:w-4 place-content-center'>
                     <icon.icon
                       className={`${
                         activeCategory === index
@@ -98,7 +159,7 @@ const Portfolio = () => {
           </div>
           <hr className='border-[0.3px] border-gray-300 border-solid sm:order-1 md:hidden' />
           {activeCategory !== null ? (
-            <div className='flex flex-col py-5 sm:py-10 px-4 sm:px-12 gap-y-5 sm:gap-y-10 relative overflow-y-auto h-full w-full md:flex-1'>
+            <div className='flex flex-col py-5 sm:py-10 px-4 sm:px-12 gap-y-5 sm:gap-y-10 relative overflow-y-auto h-full w-full md:flex-1 bg-white/95'>
               {activeCategory === 0 &&
                 skills.map((item) => (
                   <div
@@ -399,7 +460,7 @@ const Portfolio = () => {
                 ))}
             </div>
           ) : (
-            <div className='flex items-center flex-col justify-center gap-y-2 flex-1'>
+            <div className='flex items-center flex-col justify-center gap-y-2 flex-1 bg-white/95'>
               <h2 className='text-base font-medium text-slate-800'>
                 Nothing to see here.
               </h2>
