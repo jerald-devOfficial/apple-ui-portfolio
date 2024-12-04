4b1b80ce162c0efa76d8ee9c06865371bc7f0a0c
jerald-devOfficial <spaueOfficial@gmail.com>
2024-12-04 23:24:08 +0800

    feat: fetch diary data in /diaries page

--- src/app/diaries/page.tsx
+++ src/app/diaries/page.tsx
@@ -1,5 +1,23 @@
+'use client'
+
+import { IDiary } from '@/models/Diary'
+import useSWR, { Fetcher } from 'swr'
+
 const Diaries = () => {
-  return <div>Diaries</div>
+  const fetcher: Fetcher<IDiary[], string> = (url) =>
+    fetch(url).then((res) => res.json())
+
+  const { data, error, isLoading, mutate } = useSWR('/api/diary', fetcher)
+
+  console.log('data: ', data)
+
+  return (
+    <main>
+      <div>Diaries</div>
+      {data &&
+        data.map((diary: IDiary) => <div key={diary._id}>{diary.title}</div>)}
+    </main>
+  )
 }
 
 export default Diaries
