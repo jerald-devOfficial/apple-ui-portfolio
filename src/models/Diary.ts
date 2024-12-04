06a39c513147b28b6bf66d392e24a6f2cd760df5
jerald-devOfficial <spaueOfficial@gmail.com>
2024-12-04 23:21:59 +0800

    feat: add publicity and tags in diary schema

--- src/models/Diary.ts
+++ src/models/Diary.ts
@@ -1,25 +1,37 @@
-import mongoose, { Model, model } from 'mongoose';
+import mongoose, { Model, model } from 'mongoose'
 
 export interface IDiary extends Document {
-    _id: string,
-     title: string;
-     content: string;
-      createdAt: Date;
-      updatedAt: Date;
+  _id: string
+  title: string
+  content: string
+  createdAt: Date
+  updatedAt: Date
+  publicity: boolean
+  tags?: string[]
+}
+
+const diarySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    publicity: {
+      type: Boolean,
+      default: false
+    },
+    tags: {
+      type: [String],
+      default: []
     }
-
-const diarySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true
   },
-  content: {
-    type: String,
-    required: true
-  }  },{ timestamps: true }
-);
-
+  { timestamps: true }
+)
 
 export const Diary = (mongoose.models.Diary ||
-    model('Diary', diarySchema)) as Model<IDiary>;
+  model('Diary', diarySchema)) as Model<IDiary>
