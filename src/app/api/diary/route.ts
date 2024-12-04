d01cc8664ed226c72f54bae006ab81eca49d0c3b
jerald-devOfficial <spaueOfficial@gmail.com>
2024-12-04 23:23:13 +0800

    feat: add publicity and tags in diary route POST request

--- src/app/api/diary/route.ts
+++ src/app/api/diary/route.ts
@@ -1,43 +1,43 @@
-import connect from '@/utils/db'
-import { NextResponse } from 'next/server';
-import mongoose from 'mongoose'
 import { Diary } from '@/models/Diary'
+import connect from '@/utils/db'
+import mongoose from 'mongoose'
+import { NextResponse } from 'next/server'
 
 export const POST = async (req: Request) => {
-  const { title, content } = await req.json();
+  const { title, content, publicity, tags } = await req.json()
 
   try {
-    await connect();
+    await connect()
 
-    const newDiary = await Diary.create({ title, content });
+    const newDiary = await Diary.create({ title, content, publicity, tags })
 
     return NextResponse.json({
       diary: newDiary,
       message: 'Diary created successfully',
       success: true
-    });
+    })
   } catch (error) {
     if (error instanceof mongoose.Error.ValidationError) {
-        let errorList = []
-        for (let e in error.errors) {
-          errorList.push(error.errors[e].message)
-        }
-        console.log(errorList)
-        return NextResponse.json({ msg: errorList })
-      } else {
-        return NextResponse.json({ msg: ['Unable to send message.'] })
+      let errorList = []
+      for (let e in error.errors) {
+        errorList.push(error.errors[e].message)
       }
+      console.log(errorList)
+      return NextResponse.json({ msg: errorList })
+    } else {
+      return NextResponse.json({ msg: ['Unable to send message.'] })
+    }
   }
-};
+}
 
 export const GET = async () => {
   try {
-    await connect();
+    await connect()
 
-    const diaries = await Diary.find();
+    const diaries = await Diary.find()
 
-    return new NextResponse(JSON.stringify(diaries), { status: 200 });
+    return new NextResponse(JSON.stringify(diaries), { status: 200 })
   } catch (error) {
-    return new NextResponse('Database Error', { status: 500 });
+    return new NextResponse('Database Error', { status: 500 })
   }
-};
+}
