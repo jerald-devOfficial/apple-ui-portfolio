c39199d35d8ef4b98a3a5bad7eda4e6ebf04b007
jerald-devOfficial <spaueOfficial@gmail.com>
2024-12-10 00:10:58 +0800

    feat: upgrade react and react-dom types to 19, remove autoprefixer and revert to postcss.config.mjs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,24 @@
-import type { Config } from "tailwindcss";
-const defaultTheme = require('tailwindcss/defaultTheme')
+import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     screens: {
-      'xs': '390px',
-      ...defaultTheme.screens,
+      xs: '390px',
+      ...defaultTheme.screens
     },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
